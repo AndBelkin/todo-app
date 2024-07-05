0317a68ad5140c15ebd863ef57ede5d899395902
AndBelkin <dev.andreubelkin@gmail.com>
2024-07-05 09:59:30 +0300

    Add Helmet packege

--- src/TodoApp.tsx
+++ src/TodoApp.tsx
@@ -1,7 +1,12 @@
 import { FC } from "react";
+import { Helmet } from "react-helmet";
 
 const TodoApp: FC = () => {
-  return <div className="todo-app"></div>;
+  return (
+    <div className="todo-app">
+      <Helmet title="Todo Application by AndBelkin" />
+    </div>
+  );
 };
 
 export default TodoApp;
