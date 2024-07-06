7187ad4d4c0b717246f103b4ffe3da72add9fc00
AndBelkin <dev.andreubelkin@gmail.com>
2024-07-06 08:07:03 +0300

    Add ShadowWindow.tsx & styles

--- src/TodoApp.tsx
+++ src/TodoApp.tsx
@@ -3,6 +3,7 @@ import { Helmet } from "react-helmet";
 import ItemsContainer from "./components/todoApp/ItemsContainer";
 import CreateItemsForm from "./components/todoApp/CreateItemsForm";
 import { useAppSelector } from "./app/hooks";
+import ShadowWindow from "./components/todoApp/ShadowWindow";
 
 const TodoApp: FC = () => {
   const isOpen: boolean = useAppSelector((state) => state.todoApp.isAdd);
@@ -10,6 +11,7 @@ const TodoApp: FC = () => {
     <div className="todo-app">
       <Helmet title="Todo Application by AndBelkin" />
       {isOpen ? <CreateItemsForm /> : false}
+      {isOpen ? <ShadowWindow /> : false}
       <ItemsContainer />
     </div>
   );
---
+++ src/components/todoApp/ShadowWindow.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const ShadowWindow: FC = () => {
+  return <div className="shadow-window"></div>;
+};
+
+export default ShadowWindow;
