e52ac6a637d81d6098214e5592d8a291df6359f2
AndBelkin <dev.andreubelkin@gmail.com>
2026-05-13 13:08:24 +0300

    Create the CreatePage page component

--- src/app/routes/AppRouter.tsx
+++ src/app/routes/AppRouter.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router";
-import { IndexPage, NotFoundPage } from "../../pages";
+import { CreatePage, IndexPage, NotFoundPage } from "../../pages";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route index element={<IndexPage />} />
-      <Route path="/new" element={<p>Create new TodoItem</p>} />
+      <Route path="/new" element={<CreatePage />} />
       <Route path="/item/:id">
         <Route index element={<p>Show TodoItem</p>} />
         <Route path="edit" element={<p>Edit TodoItem</p>} />
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
 export { IndexPage } from "./index/IndexPage";
+export { CreatePage } from "./create/CreatePage";
 export { NotFoundPage } from "./not-found/NotFoundPage";
