a95c2513f02b9e54fe0d1ece78614781826fb310
AndBelkin <dev.andreubelkin@gmail.com>
2026-05-14 10:03:11 +0300

    Edit the TodoItem component

--- src/entities/todo/TodoItem.tsx
+++ src/entities/todo/TodoItem.tsx
@@ -1,21 +1,27 @@
 import type { FC } from "react";
-import { IoIosCloseCircle } from "react-icons/io";
 import type { TodoItemProps } from "./model/types";
 import { useTodoItem } from "./model/hooks";
-import { convertDateToString } from "./lib/utils";
+import { PiArrowsCounterClockwiseLight as RepeatIcon } from "react-icons/pi";
+import { CiClock2 as ClockIcon } from "react-icons/ci";
 import "./TodoItem.css";
 
 export const TodoItem: FC<TodoItemProps> = ({ item }) => {
-  const { handleRemoveTodo, handleOpenTodo, handleKeyPress } = useTodoItem(item);
+  const { CheckboxIcon, handleOpenTodo, handleKeyPress, handleToggleTodo } = useTodoItem(item);
+
   return (
-    <div className="todo-item" tabIndex={0} onKeyDown={handleKeyPress}>
-      <header className="todo-item-header">
-        <IoIosCloseCircle className="todo-item-remove" onClick={handleRemoveTodo} />
-      </header>
-      <main className="todo-item-main" onClick={handleOpenTodo} title={`Open "${item.title}"`}>
-        <h5 className="todo-item-title">{item.title}</h5>
-        <p className="todo-item-createdAt">Created: {convertDateToString(item.createdAt)}</p>
-      </main>
+    <div className="todo-item" onClick={handleOpenTodo} onKeyDown={handleKeyPress} role="button" tabIndex={0}>
+      <CheckboxIcon className="todo-item-checkbox" onClick={handleToggleTodo} />
+      <div className="flex flex-col gap-1">
+        <h6 className="todo-item-title">{item.title}</h6>
+        <div className="flex items-center gap-1 text-xs">
+          <ClockIcon />
+          <p>00:00</p>
+        </div>
+        <div className="flex items-center gap-1 text-xs">
+          <RepeatIcon />
+          <p>Every day</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/entities/todo/model/hooks.ts
+++ src/entities/todo/model/hooks.ts
@@ -1,14 +1,20 @@
 import { useNavigate } from "react-router";
 import type { Todo } from "./types";
 import { useTodoStore } from "./slice";
+import { PiCheckCircleThin as CompletedIcon, PiCircleThin as CircleIcon } from "react-icons/pi";
 
 export const useTodoItem = (todo: Todo) => {
-  const { remove } = useTodoStore();
+  const { toggle } = useTodoStore();
   const navigate = useNavigate();
 
-  const handleRemoveTodo = () => remove(todo.id);
+  const CheckboxIcon = todo.completed ? CompletedIcon : CircleIcon;
+
+  const handleToggleTodo = (event: React.MouseEvent<SVGElement>) => {
+    event.stopPropagation();
+    toggle(todo.id);
+  };
   const handleOpenTodo = () => navigate(`/item/${todo.id}`);
   const handleKeyPress = (event: React.KeyboardEvent) => (event.key === "Enter" ? handleOpenTodo() : false);
 
-  return { handleRemoveTodo, handleOpenTodo, handleKeyPress };
+  return { CheckboxIcon, handleToggleTodo, handleOpenTodo, handleKeyPress };
 };
