bcc046a953201b4e4a09f380eadbda47f56bf805
AndBelkin <dev.andreubelkin@gmail.com>
2026-05-14 15:58:08 +0300

    Create the Category for TodoStore

--- src/entities/todo/model/slice.ts
+++ src/entities/todo/model/slice.ts
@@ -11,20 +11,31 @@ export const useTodoStore = create<TodoState>()(
           title: "First task",
           completed: false,
           createdAt: new Date().toISOString(),
+          categoryID: null,
         },
         {
           id: crypto.randomUUID(),
           title: "Second task",
           completed: false,
           createdAt: new Date().toISOString(),
+          categoryID: null,
         },
         {
           id: crypto.randomUUID(),
           title: "Third task",
           completed: false,
           createdAt: new Date().toISOString(),
+          categoryID: null,
         },
       ],
+      categories: [
+        {
+          id: "main",
+          name: "main",
+          color: "#777777",
+        },
+      ],
+
       create: (data) => {
         const newTodo: Todo = { ...data, id: crypto.randomUUID(), createdAt: new Date().toISOString() };
         set((state) => ({ todos: [...state.todos, newTodo] }));
@@ -42,9 +53,27 @@ export const useTodoStore = create<TodoState>()(
           todos: state.todos.map((todo) => (todo.id === id ? { ...todo, ...date } : todo)),
         })),
 
+      addCategory: (name, color) =>
+        set((state) => ({
+          categories: [...state.categories, { id: crypto.randomUUID(), name, color }],
+        })),
+
+      removeCategory: (id) =>
+        set((state) => ({
+          categories: state.categories.filter((cat) => cat.id !== id),
+        })),
+
+      updateCategory: (id, data) =>
+        set((state) => ({
+          categories: state.categories.map((cat) => (cat.id === id ? { ...cat, data } : cat)),
+        })),
+
       getAll: () => get().todos,
       getCompleted: () => get().todos.filter((todo) => todo.completed),
       getUnfinished: () => get().todos.filter((todo) => !todo.completed),
+
+      getCategories: () => get().categories,
+      getByCategory: (categoryID) => get().todos.filter((todo) => todo.categoryID === categoryID),
     }),
     {
       name: "todo-storage",
--- src/entities/todo/model/types.ts
+++ src/entities/todo/model/types.ts
@@ -1,20 +1,37 @@
+export interface Category {
+  id: string;
+  name: string;
+  color?: string;
+}
+
 export interface Todo {
   id: string;
   title: string;
   completed: boolean;
   createdAt: string;
+  categoryID: string | null;
 }
 
 export interface TodoState {
   todos: Todo[];
+  categories: Category[];
+  // Main methods
   create: (data: TodoCreateDate) => void;
   toggle: (id: string) => void;
   remove: (id: string) => void;
   update: (id: string, date: TodoUpdateDate) => void;
+  // Methods for Category
+  addCategory: (name: string, color?: string) => void;
+  removeCategory: (id: string) => void;
+  updateCategory: (id: string, date: Omit<Category, "id">) => void;
 
+  // get's methods
   getAll: () => Todo[];
   getCompleted: () => Todo[];
   getUnfinished: () => Todo[];
+
+  getByCategory: (categoryID: string) => Todo[];
+  getCategories: () => Category[];
 }
 
 export interface TodoItemProps {
