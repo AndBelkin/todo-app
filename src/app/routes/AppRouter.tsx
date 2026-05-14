c2ea80a33ccd20d3c913fabcebfb47e20f213712
AndBelkin <dev.andreubelkin@gmail.com>
2026-05-14 10:08:50 +0300

    Delete the CreatePage

--- src/app/routes/AppRouter.tsx
+++ src/app/routes/AppRouter.tsx
@@ -1,11 +1,10 @@
 import { Route, Routes } from "react-router";
-import { CreatePage, IndexPage, NotFoundPage } from "../../pages";
+import { IndexPage, NotFoundPage } from "../../pages";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route index element={<IndexPage />} />
-      <Route path="/new" element={<CreatePage />} />
       <Route path="/item/:id">
         <Route index element={<p>Show TodoItem</p>} />
         <Route path="edit" element={<p>Edit TodoItem</p>} />
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,2 @@
 export { IndexPage } from "./index/IndexPage";
-export { CreatePage } from "./create/CreatePage";
 export { NotFoundPage } from "./not-found/NotFoundPage";
